1c3bec8a69690ed55d73648d60916b0dea4add4d
PERRIER <maxperrier87@gmail.com>
2023-03-19 16:39:08 +0100

    + Page d'ajout de tâche

--- app/src/main/java/com/example/todolistkotlin/MainActivity.kt
+++ app/src/main/java/com/example/todolistkotlin/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.todolistkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var addbutton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView);
+        addbutton = findViewById(R.id.addbutton);
+        addbutton.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddActivities::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
